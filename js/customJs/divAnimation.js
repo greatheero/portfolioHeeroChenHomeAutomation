4104d2fc68f8fc1321119fa860ad1a5b4eaf2b5e
HeeroYPChen <greatheero@gmail.com>
2017-10-29 09:45:54 +0800

    updated for serious design mind showcase
    1. update several images and icons
    2. update animation transition

--- js/customJs/divAnimation.js
+++ js/customJs/divAnimation.js
@@ -40,27 +40,27 @@ $.fn.isInViewport = function() {
 };
 
 $(window).on('load resize scroll', function() {
-  // $('.section-primary').each(function() {
-  //   var elementId = $(this).attr('id');
-  //
-  //   if ($(this).isInViewport()) {
-  //     $(".pulse").addClass("pulseGrow");
-  //     $(".diamond").addClass("diamondDrop");
-  //     $(".title" + elementId).addClass("sectionTitleSlideIn");
-  //     $(".icon" + elementId).addClass("sectionIconBubbleUp");
-  //     $(".text" + elementId).addClass("sectiontextShowUp");
-  //     $("#button" + elementId).addClass("sectionAnimatedButton");
-  //     $("#text" + elementId).addClass("sectionAnimatedText");
-  //     for (var i = 0; i < 3; i++) {
-  //       $("#imageFrame" + elementId + "_" + i.toString()).addClass("animatedImageFrameLevel_" + i.toString());
-  //     }
-  //
-  //   } else {
-  //     for (var i = 0; i < 3; i++) {
-  //       $("#imageFrame" + elementId + "_" + i.toString()).removeClass("animatedImageFrameLevel_" + i.toString());
-  //     }
-  //   }
-  // });
+  $('.section-primary').each(function() {
+    var elementId = $(this).attr('id');
+
+    if ($(this).isInViewport()) {
+      $(".pulse").addClass("pulseGrow");
+      $(".diamond").addClass("diamondDrop");
+      $(".title" + elementId).addClass("sectionTitleSlideIn");
+      $(".icon" + elementId).addClass("sectionIconBubbleUp");
+      $(".text" + elementId).addClass("sectiontextShowUp");
+      $("#button" + elementId).addClass("sectionAnimatedButton");
+      $("#text" + elementId).addClass("sectionAnimatedText");
+      for (var i = 0; i < 3; i++) {
+        $("#imageFrame" + elementId + "_" + i.toString()).addClass("animatedImageFrameLevel_" + i.toString());
+      }
+
+    } else {
+      for (var i = 0; i < 3; i++) {
+        $("#imageFrame" + elementId + "_" + i.toString()).removeClass("animatedImageFrameLevel_" + i.toString());
+      }
+    }
+  });
 
   $('.section-secondary').each(function() {
     var elementId = $(this).attr('id');
